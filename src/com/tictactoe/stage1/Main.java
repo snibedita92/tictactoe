2072a79c2b123579e2995873119aa01375cf0228
RashmiRB <rbidanta@yahoo.in>
2022-06-07 23:53:35 -0500

    Refactored code

--- src/com/tictactoe/stage1/Main.java
+++ src/com/tictactoe/stage1/Main.java
@@ -1,4 +1,4 @@
-package tictactoe;
+package com.tictactoe.stage1;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/com/tictactoe/stage2/Main.java
@@ -0,0 +1,4 @@
+package com.tictactoe.stage2;
+
+public class Main {
+}
